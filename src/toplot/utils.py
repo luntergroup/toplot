92059d484ee188b58106239dc414cfa838051a5c
Dorien Neijzen <dneijzen@gmail.com>
2024-06-17 10:18:18 +0200

    added function to convert numpy array to two-level dataframe

--- src/toplot/utils.py
+++ src/toplot/utils.py
@@ -10,3 +10,19 @@ def dataframe_to_pytree(df: pd.DataFrame, to_numpy=True) -> dict:
     if to_numpy:
         pytree = tree.map(lambda x: x.to_numpy(), pytree)
     return pytree, metadata
+
+def array_to_twolevel_dataframe(array, column_name_dictionary):
+    """Convert a numpy array to a two-level dataframe.
+    array: {np.array} of dimensions [samples, words]
+    column_name_dictionary: dictionary of format {featurename: [words]}, thus dictionary with lists
+    """
+
+    two_level_data_dictionary = {}
+    for f, (featurename, words) in enumerate(column_name_dictionary.items()):
+        if f < np.shape(array)[1]:
+            print(f, words)
+            two_level_data_dictionary[featurename] = pd.DataFrame(
+                array[:, f : f + len(words)], columns=words
+            )
+    two_level_data_frame = pd.concat(two_level_data_dictionary, axis="columns")
+    return two_level_data_frame
