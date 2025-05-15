09ba73536ed908d0ca7e7efc91bbc65cdcec5f73
Hylke Donker <h.c.donker@umcg.nl>
2025-05-15 14:05:26 +0200

    Expose hinton to main module

--- src/toplot/__init__.py
+++ src/toplot/__init__.py
@@ -1,3 +1,3 @@
 from .hidden import plot_cohort, plot_polar_cohort
 from .scattermap import scattermap
-from .weights import bar_plot, bar_plot_stacked, scattermap_plot
+from .weights import bar_plot, bar_plot_stacked, hinton, scattermap_plot
